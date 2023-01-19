88bb92636d7fdc1634ceb26e37cfdb2b9275995b
CorrioneroFederico <corrionerofederico@gmail.com>
2023-01-18 22:04:44 -0300

    implementacion de rutas

--- src/app/protected/protected-routing.module.ts
+++ src/app/protected/protected-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    children: [
+    {path: 'dashboard', component: DashboardComponent},
+    {path: '**', redirectTo: ''}]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
